097d80945125375c7754a70273cc72bac5eb5586
7kooter1st <119578139+7kooter1st@users.noreply.github.com>
2025-03-20 09:07:04 +0300

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,18 @@
-document.getElementById("btn_theme").onclick(function(){
-    document
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('btn-theme');
+    let theme = 0;
+    if (button) {
+        button.addEventListener('click', function() {
+            const body = document.body;
+            if (theme == 0) {
+                body.style.backgroundColor = 'white';
+                theme = 1;
+            } else {
+                body.style.backgroundColor = '#0f0f0f';
+                theme = 0;
+            }
+        });
+    } else {
+        console.error('Кнопка с id "btn-theme" не найдена.');
+    }
+});
